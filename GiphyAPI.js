19c83b70e51c70f268f489083621b13de574b5c8
kimberly lin <kimberly.lin10@gmail.com>
2022-01-26 01:01:26 -0800

    Cleaned up commented out code

--- GiphyAPI.js
+++ GiphyAPI.js
@@ -1,19 +1,12 @@
 import axios from "axios";
 
 const GIPHY_API_URL = "https://api.giphy.com/v1/gifs/trending?"
-// const GIPHY_API_KEY = process.env.GIPHY_API_KEY;
 const GIPHY_API_KEY = "Awq5410QQl0416nJogqlsinldM2s9PCA"
 
-/** GiphyAPI Class for making axios call to GiphyAPI
- *
- */
-
+/** GiphyAPI Class for making axios call to GiphyAPI*/
 class GiphyApi {
 
-  // Individual API routes
-
-  /** Get top (limit #) of trending GIFs and return [url1, url2, ...]*/
-
+  /** Get top (limit #) of trending GIFs and return [url1, url2, ...] */
   static async getTrending(limit=50) {
     try {
       let response = await axios.get(`${GIPHY_API_URL}api_key=${GIPHY_API_KEY}&limit=${limit}`);
